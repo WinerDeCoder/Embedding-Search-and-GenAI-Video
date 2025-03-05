ad970285922ac0f937fda28132a730a281c47c82
Phuc Nguyen Phuoc Nguyen <nguyenphuc100402@gmail.com>
2025-03-05 07:39:15 +0000

    api

---
+++ test/api/heygen/app/main.py
@@ -0,0 +1,154 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import time
+import requests
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+app = FastAPI()
+
+
+# Add CORS middleware to the FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # React's URL   localhost
+    allow_credentials=False,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+#Payload
+def payload_setting(title, script):
+    
+    # payload = {
+    #     "test": True,
+    #     "caption": False,
+    #     "title": title,
+    #     "dimension": {
+    #             "width": 1920,  # Fix
+    #             "height": 1080  # fix
+    #         },
+    #     "variables": {
+    #         "script": {
+    #             "name": "script",
+    #             "type": "text",
+    #             "properties": {"content": script},
+    #         }
+    #     },
+    # }
+    
+    payload = {
+            "caption": False,  # Caption, maybe the subtile
+            "title": title,  # Need to change , match the index of video
+            "callback_id": "string",
+            "dimension": {
+                "width": 1024,  # Fix
+                "height": 720  # fix
+            },
+            "video_inputs": [      # up to 50, don't know whether it is video setting or many video at a time
+                {
+                    "character": {
+                        "type": "avatar",
+                        "avatar_id": "Angela-inTshirt-20220820",
+                        "avatar_style": "normal"
+                    },
+                    "voice": {
+                        "type": "text",
+                        "voice_id": "c6fb81520dcd42e0a02be231046a8639", #str
+                        "input_text": script #str
+                    },   # voice id Fixx
+                }
+            ],
+            "callback_url": "string"
+        }
+    
+    return payload
+
+
+# General Define
+heygen_api_key  = os.getenv("HEYGEN_API_KEY")
+headers         = {"Accept": "application/json", "X-API-KEY": heygen_api_key}
+template_id     = os.getenv("TEMPLATE_ID")
+
+#generate_url    = f"https://api.heygen.com/v2/template/{template_id}/generate"
+generate_url    = "https://api.heygen.com/v2/video/generate"
+
+class QARequest(BaseModel):
+    no: str
+    question: str
+    answer: str
+
+class VideoResponse(BaseModel):
+    video_url: str
+    
+    
+    
+def video_generator(title, script):
+    
+    payload = payload_setting(title, script)
+    
+    headers["Content-Type"] = "application/json"
+    response = requests.post(generate_url, headers=headers, json=payload)
+    if not response.json()["data"]:
+        print(response)
+        print(response.json()["error"])
+        
+        return title, "error", ""
+        
+
+    video_id = response.json()["data"]["video_id"]
+    print("video_id:", video_id)
+    
+    
+    # Check Video Generation Status
+    video_status_url = f"https://api.heygen.com/v1/video_status.get?video_id={video_id}"
+    while True:
+        response = requests.get(video_status_url, headers=headers)
+        status = response.json()["data"]["status"]
+
+        if status == "completed":
+            video_url = response.json()["data"]["video_url"]
+            thumbnail_url = response.json()["data"]["thumbnail_url"]
+            # print(
+            #     f"Video generation completed! \nVideo URL: {video_url} \nThumbnail URL: {thumbnail_url}"
+            # )
+
+            # Save the video to a file
+            # video_filename = f"../../data/videos/{title}.mp4"
+            # with open(video_filename, "wb") as video_file:
+            #     video_content = requests.get(video_url).content
+            #     video_file.write(video_content)
+                
+            return title, status, video_url
+
+        elif status == "processing" or status == "pending":
+            print("Video is still processing. Checking status...")
+            time.sleep(10)  # Sleep for 5 seconds before checking again
+        elif status == "failed":
+            error = response.json()["data"]["error"]
+            print(f"Video generation failed. '{error}'")
+            
+            return title, status, ""
+    
+    
+@app.get("/")
+async def root():
+    return {"message": "API is ready!"}
+
+
+@app.post("/generate_video", response_model=VideoResponse)
+def generate_video(request: QARequest):
+    # Process function logic goes here
+    no = request.no 
+    question = request.question
+    answer = request.answer
+    
+    video_id, video_status, video_url = video_generator(no, answer)
+    
+    
+    
+    
+    
+    return VideoResponse(video_url=video_url)
